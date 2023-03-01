6bbce649f0488470a8bb32f182d1b67b4287d722
Rezaul Karim RK <55434814+rezaulkarimrk@users.noreply.github.com>
2023-03-01 22:37:13 +0600

    Add files via upload

---
+++ second_problem.php
@@ -0,0 +1,36 @@
+<?php
+function stringCheck( $string ){
+
+	$len = strlen( $string );
+    if( $len%2 == 1 ){
+		return false;
+	}
+	$last = $len-1;
+	for( $i=0; $i<=($len/2)-1; $i++){
+        if( ($string[$i] == '(' ) && ($string[$last] == ')' )){
+            $last--;
+        }
+        else if(($string[$i] == '{' ) && ($string[$last] == '}' )){
+            $last--;
+            continue;
+        }
+        else if(($string[$i] == '[' ) && ($string[$last] == ']' )){
+            $last--;
+            continue;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
+
+$string = "(([{[(]]}]))";
+
+$result = stringCheck($string);
+if($result){
+    echo 'true';
+}
+else{
+    echo 'false';
+}
